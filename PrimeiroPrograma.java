46f771ad071a351191fb59a89ce7bc24c19552fd
Carolina Soares Moreira <74939095+carolinamoreira13@users.noreply.github.com>
2022-07-20 14:31:20 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.dio.com;
+
+public class Gato {
+  Gato gato = new Gato (cor, cor, idade) ;
+	public static void main(String[] args) {
+		);
+		private String nome ;
+		private String cor ;
+		private Integer idade ;
+		
+		public Gato (String nome, String cor, Integer idade) }
+	this.nome
+
+System.out.println (gato);
